85101d1e703c69abc68df32aad2d4eaf662223ff
fernandoprestes <fernandoprestesc@gmail.com>
2022-02-10 14:49:05 -0300

    add about pages for route

---
+++ pages/AboutPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <section
+      class="flex flex-col-reverse items-center w-full mb-16 rounded-md xl:container md:mx-auto md:my-0 sm:mx-auto sm:my-0 lg:flex-row"
+    >
+      <h1 class="p-4">aboutpage</h1>
+    </section>
+  </main>
+</template>
+<script>
+export default {
+  name: 'AboutPage',
+  layout: 'DefaultLayout',
+}
+</script>
